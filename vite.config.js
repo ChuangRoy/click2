706fe270bd73a7598449dd26c06352e9288011f5
Roy Chuang <roy0702.chuang@gmail.com>
2025-05-24 21:45:50 +0800

    might be

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    // base: '/click2/',
+    base: '/click2/',
     plugins: [react()],
 })
